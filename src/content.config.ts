5de0a69d0dd43c4df5877c3f6eb2ba22587150d3
Andrew Lee Rubinger <alr@alrubinger.com>
2025-03-25 02:28:25 -0700

    Start a Blog feature using Astro content collections and Starlight consistent styling

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,7 +1,16 @@
-import { defineCollection } from 'astro:content';
+import { z, defineCollection } from 'astro:content';
 import { docsLoader } from '@astrojs/starlight/loaders';
 import { docsSchema } from '@astrojs/starlight/schema';
+import { glob, file } from 'astro/loaders';
 
 export const collections = {
+  // Docs are driven by Starlight
   docs: defineCollection({ loader: docsLoader(), schema: docsSchema() }),
+  // Our Blog Implementation is an Astro Content Collection
+  blog: defineCollection({
+    loader: glob({ pattern: "**/*.mdx", base: "./src/blog" }),
+    schema: z.object({
+      title: z.string(),
+      pubDate: z.coerce.date(),
+    })})
 };
