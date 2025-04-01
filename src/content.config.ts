0e9f9debd4643f4ff59e8faf06ef47d314c24afc
Andrew Lee Rubinger <alr@alrubinger.com>
2025-03-31 18:47:16 -0700

    Add an authors database and surface in an Author component

--- src/content.config.ts
+++ src/content.config.ts
@@ -8,9 +8,21 @@ export const collections = {
   docs: defineCollection({ loader: docsLoader(), schema: docsSchema() }),
   // Our Blog Implementation is an Astro Content Collection
   blog: defineCollection({
-    loader: glob({ pattern: "**/*.mdx", base: "./src/content/blog" }),
+    loader: glob({ pattern: "**/*.mdx", base: "./src/content/blog/posts" }),
     schema: z.object({
       title: z.string(),
+      author: z.string(),
       pubDate: z.coerce.date(),
-    })})
+    })}),
+  // Blog Authors are also an Astro Content Collection
+  //TODO Make some properties optional
+  authors: defineCollection({
+    loader: file("./src/content/blog/authors.json"),
+    schema: z.object({
+      name: z.string(),
+      title: z.string(),
+      'social-github': z.string(),
+      'social-linkedin': z.string(),
+      'social-x': z.string(),
+    })}),
 };
