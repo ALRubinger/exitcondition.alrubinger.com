346abbceff6427618994752cea8f814fed4c71b0
Andrew Lee Rubinger <alr@alrubinger.com>
2025-06-24 06:13:46 -0700

    Improvements to blog index page and snippets

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,12 +1,32 @@
-import { defineCollection, z } from 'astro:content';
+import { z, defineCollection } from 'astro:content';
+import { docsLoader } from '@astrojs/starlight/loaders';
 import { docsSchema } from '@astrojs/starlight/schema';
+import { glob, file } from 'astro/loaders';
 
 export const collections = {
-  docs: defineCollection({
-    schema: docsSchema({
-      extend: z.object({
-        hideHeader: z.boolean().optional(),
-      }),
-    }),
-  }),
+  // Docs are driven by Starlight
+  docs: defineCollection({ loader: docsLoader(), schema: docsSchema() }),
+  // Our Blog Implementation is an Astro Content Collection
+  blog: defineCollection({
+    loader: glob({ pattern: "**/*.mdx", base: "./src/content/blog/posts" }),
+    schema: z.object({
+      title: z.string(),
+      author: z.string(),
+      pubDate: z.coerce.date(),
+      snippet: z.string().optional(),
+      heroImage: z.string().optional(),
+      tags: z.array(z.string()).optional(),
+    })}),
+  // Blog Authors are also an Astro Content Collection
+  //TODO Make some properties optional
+  authors: defineCollection({
+    loader: file("./src/content/blog/authors.json"),
+    schema: z.object({
+      name: z.string(),
+      title: z.string(),
+      'social-github': z.string(),
+      'social-linkedin': z.string(),
+      'social-x': z.string(),
+      avatar: z.string(),
+    })}),
 };
